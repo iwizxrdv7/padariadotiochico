2c9e07b8b624945fd6c27adc1cd8c99840cef6bd
iwizxrdv7 <iwizxrd.7@gmail.com>
2025-11-10 19:09:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -292,19 +292,8 @@ function updateTotalsUI() {
     if (btn) btn.textContent = `Confirmar Pedido no valor de ${money(final)}`;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  const confirmBtn = document.getElementById("confirm-btn");
-
-  confirmBtn.addEventListener("click", async () => {
-    console.log("BOTÃO FUNCIONOU ✅");
-
-    document.getElementById("pix-modal").style.display = "flex";
-    iniciarTimer();
-  });
-});
-
-
 document.addEventListener('DOMContentLoaded', updateTotalsUI);
+
 
 
 
